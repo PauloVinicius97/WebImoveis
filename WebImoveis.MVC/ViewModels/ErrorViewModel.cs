2d8846a4e3d574182a40c984b084add40fdd6295
Paulo Vinícius <paulosantoscosta97@gmail.com>
2022-04-19 11:54:09 -0300

    Deletando um diretório que nem deveria estar aqui

--- WebImoveis.MVC/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace WebImoveis.MVC.Models {
-    public class ErrorViewModel {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
